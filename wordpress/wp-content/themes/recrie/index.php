416a0d75bd6e83bb2121e22e0ad546d53ea6d2ea
Wesley <ewesleymanon@gmail.com>
2019-10-31 11:08:08 +0800

    added toppage sp

--- wordpress/wp-content/themes/recrie/index.php
+++ wordpress/wp-content/themes/recrie/index.php
@@ -31,7 +31,8 @@
     </div>
   </div>
 
-  <div class="p-service" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/minified/images/service.png')">
+  <div class="p-service" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/minified/images/service.jpeg')">
+    <div class="p-service__bg"></div>
     <div class="p-service__arrow-down u-center-block"></div>
     <p class="p-service__detail">
     弊社の財務コーチングは、そんな経営に関する悩みを払拭し、契約満了時には、以下の状況を手に入れる事が可能な、御社の為だけの唯一無二のカスタムメイド・コーチングです。
@@ -67,14 +68,14 @@
   </div>
   <div class="p-coaching">
     <p class="p-coaching__title">弊社財務コーチングで獲得できる事項</p>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/vertical-icon.png" alt="" srcset="" class="p-center-block">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/vertical-icon.png" alt="" srcset="" class="p-center-block l-logo">
     <!-- 1st item -->
     <div class="p-coaching__item">
       <div class="p-coaching__item-visual">
         <div class="p-coaching__item-bg"></div>
         <img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/coaching-img1.png" alt="" srcset="" class="p-coaching__item-img">
       </div>
-      <div>
+      <div class="p-coaching__item-vtext">
         <p class="p-coaching__item-title">的確な現状把握が可能に</p>
         <div class="p-coaching__item-details">
           <div class="p-coaching__item-line"></div>
@@ -95,7 +96,7 @@
     </div>
     <!-- 2nd item -->
     <div class="p-coaching__item">
-      <div class="p-coaching__item-vtext">
+      <div class="p-coaching__item-vtext p-coaching__item-vtext--end">
         <p class="p-coaching__item-title">経営者が物心ともに満足できる状態に！</p>
         <div class="p-coaching__item-details">
           <div class="p-coaching__item-line"></div>
@@ -124,8 +125,8 @@
         <div class="p-coaching__item-bg"></div>
         <img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/coaching-img3.png" alt="" srcset="" class="p-coaching__item-img">
       </div>
-      <div>
-        <p class="p-coaching__item-title">目指すべき経営の目標数字が明確になり、正しい事業計画を立てることができる</p>
+      <div class="p-coaching__item-vtext">
+        <p class="p-coaching__item-title">目指すべき経営の目標数字が明確になり、<br/>正しい事業計画を立てることができる</p>
         <div class="p-coaching__item-details">
           <div class="p-coaching__item-line"></div>
           <img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/check-symbol.png" alt="" srcset="" class="">
@@ -147,7 +148,7 @@
   <!-- Coaching experience -->
   <div class="p-experience">
     <p class="p-experience__title">財務コーチングの実績</p>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/vertical-icon.png" alt="" srcset="" class="p-center-block">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/vertical-icon.png" alt="" srcset="" class="p-center-block l-logo">
     <p class="p-experience__subtitle">
     レクリエと共に財務改善に取り組んでこられたお客様の事例をご紹介します
     </p>
@@ -250,19 +251,19 @@
     <!-- Feature 1 -->
     <div class="p-three__feature-item">
       <div class="p-three__feature-item-details">
-        <div class="p-three__feature-item-title">
+        <div class="p-three__feature-item-title p-three__feature-item-title--one">
           <p class="p-three__feature-item-no">1</p>
           <p class="p-three__feature-item-text">伴走型<span class="xs">の</span><span class="medium">サポート</span></p>
         </div>
-        <p class="p-three__feature-item-content">調達再編スキームを用いて資金繰り改善を行なうだけではなく、経営者に寄り添った、悩み解決型のサービスを提供します。経営者の本質的成功を目指し、社長自身が経営改善に関する専門的スキル・知識を習得することを目的とし、一時的な改善ではなく、中長期的な改善にまでコミットしたコンサルティングの提供となります。</p>
+        <p class="p-three__feature-item-content">調達再編スキームを用いて資金繰り改善を行なうだけではなく、経営者に寄り添った、悩み解決型のサービスを提供します。<br/>経営者の本質的成功を目指し、社長自身が経営改善に関する専門的スキル・知識を習得することを目的とし、一時的な改善ではなく、中長期的な改善にまでコミットしたコンサルティングの提供となります。</p>
       </div>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/financial-coaching-img1.png" alt="" srcset="" class="p-three__feature-item-img">      
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/financial-coaching-img1.jpeg" alt="" srcset="" class="p-three__feature-item-img">      
     </div>
     <!-- Feature 2 -->
     <div class="p-three__feature-item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/financial-coaching-img2.png" alt="" srcset="" class="p-three__feature-item-img">      
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/financial-coaching-img2.jpeg" alt="" srcset="" class="p-three__feature-item-img">      
       <div class="p-three__feature-item-details">
-        <div class="p-three__feature-item-title">
+        <div class="p-three__feature-item-title p-three__feature-item-title--two">
           <p class="p-three__feature-item-no">2</p>
           <p class="p-three__feature-item-text">CFO<span class="xs">（最高財務責任者）</span><span class="medium">を</span><br/><span class="large">社外</span><span class="ms">に</span><span class="large">持</span><span class="small"><span class="large">つ</span>ことができる</span></p>
         </div>
@@ -270,9 +271,9 @@
       </div>
     </div>
     <!-- Feature 3 -->
-    <div class="p-three__feature-item">
+    <div class="p-three__feature-item p-three__feature-item--three">
       <div class="p-three__feature-item-details">
-        <div class="p-three__feature-item-title">
+        <div class="p-three__feature-item-title p-three__feature-item-title--three">
           <p class="p-three__feature-item-no">3</p>
           <p class="p-three__feature-item-text"><span class="fs39">中小企業</span><span class="fs33">の</span><br/><span class="medium">課題解決</span><span class="xs">を</span>
           <span class="large">ワンストップで</span><br/><span class="fs24">（資金繰り・集客・人材）</span></p>
