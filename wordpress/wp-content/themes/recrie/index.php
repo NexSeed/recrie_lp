a6320a10b3b9aa728b4f3f84f70e994e2b72714f
Louie Jay Albarico <43232812+lowejay@users.noreply.github.com>
2019-11-06 15:29:19 +0800

    Re-push branch to fix merge errors

--- wordpress/wp-content/themes/recrie/index.php
+++ wordpress/wp-content/themes/recrie/index.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Recrie</title>
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 	<?php wp_head(); ?>
 </head>
 
