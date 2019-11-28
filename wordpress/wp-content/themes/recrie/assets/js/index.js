33b9e4f7ab1e18cc22105349d57e18e830402378
Wesley <ewesleymanon@gmail.com>
2019-11-28 17:01:11 +0800

    added accordion and fixes on ie

--- wordpress/wp-content/themes/recrie/assets/js/index.js
+++ wordpress/wp-content/themes/recrie/assets/js/index.js
@@ -9,18 +9,30 @@ $('#toggleHamburger, #toggleHamburger2').click(function () {
 	if (flag) {
 		$('.l-hamburger').addClass('l-hamburger--toggled');
 		$('.menu-icon').attr({
-			"src": `${templateDirTest}/assets/minified/images/x-icon.png`
+			"src": templateDirTest+'/assets/minified/images/x-icon.png'
 		}).toggleClass('menu-icon--rotate90');
 		$('body').css('overflow', 'hidden');
 		$('#menu-list').slideDown(500);
 	} else {
 		$('.l-hamburger').removeClass('l-hamburger--toggled');
 		$('.menu-icon').attr({
-			"src": `${templateDirTest}/assets/minified/images/menu.png`
+			"src": templateDirTest+'/assets/minified/images/menu.png'
 		}).toggleClass('menu-icon--rotate90');
 		$('body').css('overflow', 'auto');
 		$('#menu-list').slideUp(500);
 	}
+})
+
+// Accordion on Financial Sample
+$('.p-financial_sample__btn-open').on('click', function () {
+	$('.p-financial_sample__content').slideDown(1000);
+	$(this).parent().css("display", "none");
+});
+
+$('.p-financial_sample__btn-close').on('click', function () {
+	$('.p-financial_sample__content').slideUp(1000);
+	$('.p-financial_sample__btn-prev').css("display", "block");
+
 })
 
 window.addEventListener('scroll', () => {
@@ -47,7 +59,7 @@ window.addEventListener('load', function () {
 		}
 
 	$('.cd-upload-btn').addClass('c-button__default c-button__default__upload');
-	$('.codedropz-upload-inner').prepend(`<img class='c-file__select' src='${templateDirTest}/assets/minified/images/select-file-icon.png' />`);
+	$('.codedropz-upload-inner').prepend("<img class='c-file__select' src="+templateDirTest+"/assets/minified/images/select-file-icon.png"+ "/>");
 	$('.codedropz-upload-inner > h3, .codedropz-upload-inner > span ').addClass('c-file__text');
 	$('.codedropz-upload-handler').addClass('c-file__upload');
 	mdCheck(md) // check browser resolution == 786px
@@ -77,7 +89,7 @@ function upload_errors() {
 function mdCheck(md) {
 	if (md.matches) { // If media query matches max-width of 786px
 		$('.menu-icon').attr({
-			"src": `${templateDirTest}/assets/minified/images/menu.png`
+			"src": templateDirTest+'/assets/minified/images/menu.png'
 		})
 		if (flag) {
 			$('.menu-icon').toggleClass('menu-icon--rotate90');
