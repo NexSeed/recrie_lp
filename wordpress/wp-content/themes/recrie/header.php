33881fe2c26b16974e65310b0b1ca4b377bb02f7
nexseedcamille <ykamimori@nexseed.net>
2019-11-28 19:41:49 +0800

    fix header.php

--- wordpress/wp-content/themes/recrie/header.php
+++ wordpress/wp-content/themes/recrie/header.php
@@ -7,7 +7,7 @@
 	<title>Recrie</title>
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 	<?php wp_head(); ?>
-	<?php if(!is_home() || !is_front_page() ): ?>
+	<?php if(is_page('thanks') || is_page('upload-thanks') ): ?>
   <style>
     .l-footer {
 			position: absolute;
@@ -22,12 +22,10 @@
 	<div class="l-body-container">
 		<div class="l-header-container">
 			<div class="l-navbar">
-				<a href="<?php echo home_url(); ?>" class="l-navbar__link"><img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/logo.png" alt="Company Logo" class="l-logo">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/logo.png" alt="Company Logo" class="l-logo">
 					<div class="l-navbar__title">
 						レクリエの財務改善コーチング
 					</div>
-				</a>
 			</div>
 		</div>
 		<a href="#top-link"><img class="c-fab" src="<?php echo get_template_directory_uri(); ?>/assets/minified/images/top-fab.png" alt="" srcset=""></a>
-	<?php endif;?>
