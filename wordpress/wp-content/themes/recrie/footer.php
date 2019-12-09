5072818386cc5fb66cc98073a32d8728bdddfa44
Wesley <ewesleymanon@gmail.com>
2019-12-09 10:26:34 +0800

    removed thumbnail

--- wordpress/wp-content/themes/recrie/footer.php
+++ wordpress/wp-content/themes/recrie/footer.php
@@ -6,7 +6,6 @@
       <script src="https://kit.fontawesome.com/125087969d.js" crossorigin="anonymous"></script>
       <script src="<?php echo get_template_directory_uri(); ?>/assets/js/index.js"></script>
       <script src="<?php echo get_template_directory_uri(); ?>/assets/js/lightgallery.min.js"></script>
-      <script src="<?php echo get_template_directory_uri(); ?>/assets/js/lg-thumbnail.min.js"></script>
       <script src="<?php echo get_template_directory_uri(); ?>/assets/js/lg-fullscreen.min.js"></script>
       <script src="<?php echo get_template_directory_uri(); ?>/assets/js/lg-pager.min.js"></script>
       <script src="<?php echo get_template_directory_uri(); ?>/assets/js/lg-zoom.min.js"></script>
